d4732071e46268698f92d2f35545c0b87893100d
shanthan1235 <skiru424@gmail.com>
2022-08-29 08:06:59 +0530

    first commit

--- index.js
+++ index.js
@@ -1,28 +1,74 @@
-const isValid =s =>{
-    if(s.length % 2!==0) return false;
+const numbers =  [47,84,75,21,14,14,79];
+
+// Median
+function median(arr) {
+  const arrSorted = arr.sort((a, b) => a - b);
+  return arrSorted.length % 2 === 0 ? (arrSorted[arrSorted.length/2 - 1] + arrSorted[arrSorted.length/2]) / 2 : arrSorted[Math.floor(arrSorted.length/2)];
+}
+const flatten =(values)=>values.reduce(
+  (acc, item, index, Array)=> acc.concat(
+    Array.isArray(item)? flatten(item)
+    : item
+  ),[]
+)
+
+ //highest value
+let maximum =-Infinity
+for(let number of numbers){
+  if(number>maximum)
+  maximum=number
+}
   
   
-    const stack = [];
-    const map = new Map([
-      ['(', ')'],
-      ['[', ']'],
-      ['{', '}']
-    ]);
+// Mode
+function mode(arr) {
+  const frequencyTable = {};
+  arr.forEach(elem => frequencyTable[elem] = frequencyTable[elem] + 1 || 1);
+
+  let modes = [];
+  let maxFrequency = 0;
+  for(const key in frequencyTable) {
+    if(frequencyTable[key] > maxFrequency) {
+  		modes = [Number(key)];
+  		maxFrequency = frequencyTable[key];
+  	}
+  	else if(frequencyTable[key] === maxFrequency) {
+  		modes.push(Number(key));
+  	}
+  }
   
-    console.log('s :',s)
-  
-    for (let i = 0 ; i < s.length ; i += 1) {
-      
-      if (map.has(s[i])) {
-        stack.push(map.get(s[i]));
-      } else if (s[i] !== stack.pop()) {
-        return false;
-      } 
-    }
-    return stack.length === 0;
-  };
-  console.log(isValid('()'))
-  console.log(isValid('()[]{}'))
-  console.log(isValid('(]'))
-  
-  
+  if(modes.length === Object.keys(frequencyTable).length) modes = [];
+
+  return modes;
+}
+
+
+//findprimes
+function findprimes(arr){
+    let sortArray = [];
+    arr.forEach((num)=>{
+      let factor = 0;
+      for(let i = 0; i <= num;i++){
+        if((num%i) === 0 ){
+            factor++
+
+        }
+      }
+      if(factor === 2){
+        sortArray.push(num)
+      }
+    })
+    console.log(sortArray);
+}
+
+
+
+
+
+
+console.log(`Data: ${numbers}`);
+console.log(`Median: ${median(numbers)}`);
+console.log(maximum)
+console.log(`Mode: ${mode(numbers)}`);
+findprimes([47, 84, 75, 21, 14, 14, 79])
+
